b079952bf39f9aeb10fe05f7f8ee5df1ce9ce38e
Mr P <nphap0202@gmail.com>
2024-07-21 20:04:31 -0700

    Integrating Play with React basic

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import Axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
+    const [todo, setTodo] = useState("");
+    const fetchTodoData = () => {
+        Axios.get("http://localhost:9000/todo/1").then((res) => {
+            setTodo(res.data.description);
+        });
+    };
+    useEffect(() => {
+        fetchTodoData();
+    }, []);
 
-    </div>
-  );
+// fetch("http://localhost:9000/todo/1").then((res)=>res.json()).then((data)=>{
+//     console.log(data)
+// });
+    // const User = () => {
+    //     return (
+    //         <div>
+    //             <h1>Phap</h1>
+    //             <h12>39</h12>
+    //             <h3>np@gmail.com</h3>
+    //         </div>
+    //
+    //     );
+    // }
+    return (
+        <div className="App">
+            <p>{todo}</p>
+            {/*<User/>*/}
+        </div>
+    );
 }
 
 export default App;
